4f5ef07ad70f9d33452be19ff0698247b7e1d657
Sruli <srulirapps@gmail.com>
2018-12-28 14:54:01 +0200

    implement empty state for editor with draftjs

---
+++ client/src/scenes/Notebook/components/NotebookEditor/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Editor, EditorState } from 'draft-js';
+import 'draft-js/dist/Draft.css';
+import styles from './NotebookEditor.module.scss';
+import './DraftEditorStyles.scss';
+
+class NotebookEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = editorState => this.setState({ editorState });
+    this.draftEditor = React.createRef();
+  }
+
+  componentDidMount() {
+    this.draftEditor.current.focus();
+  }
+
+  render() {
+    const { editorState } = this.state;
+
+    return (
+      <div className={`${styles.editor} ls-1`}>
+        <Editor
+          editorState={editorState}
+          onChange={this.onChange}
+          placeholder="Start typing something…"
+          ref={this.draftEditor}
+          spellCheck
+        />
+      </div>
+    );
+  }
+}
+
+export default NotebookEditor;
--- client/src/scenes/Notebook/index.js
+++ client/src/scenes/Notebook/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import NotebookEditor from './components/NotebookEditor';
 import NameSection from './components/NameSection';
 
 const Notebook = () => (
   <div className="row mt-5">
-    <div className="col-8 offset-1" />
-    <div className="col-3">
+    <div className="col-6 offset-1">
+      <NotebookEditor />
+    </div>
+    <div className="col-3 offset-2">
       <NameSection />
     </div>
   </div>
