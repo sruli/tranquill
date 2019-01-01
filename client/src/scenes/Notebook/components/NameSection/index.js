f6cb7324ded28cad943dd975587b597a3b91ac49
Sruli <srulirapps@gmail.com>
2019-01-01 14:19:36 +0200

    name field updates
    - Add notebook icon to input field area
    - updates to font color

--- client/src/scenes/Notebook/components/NameSection/index.js
+++ client/src/scenes/Notebook/components/NameSection/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './NameSection.module.scss';
+import NotebookIcon from '../../../../components/icons/NotebookIcon';
 
 const NameSection = () => (
   <React.Fragment>
-    <div className="row">
+    <div className="row align-items-center no-gutters">
+      <div className="col-1"><NotebookIcon /></div>
       <input
-        className={`col-8 form-control form-control-lg ls-1 ${styles.nameInput}`}
+        className={`col-7 form-control form-control-lg ls-1 ${styles.nameInput}`}
         type="text"
         placeholder="Notebook #1"
       />
