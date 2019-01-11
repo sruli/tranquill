44a3a413bc59e6753b8660948a3d1cdc24cd35ff
Sruli <srulirapps@gmail.com>
2019-01-11 08:07:12 +0200

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Notebook from './scenes/Notebook';
 
 const App = () => (
-  <div className="App min-vh-100">
-    <Notebook />
+  <div className="min-vh-100">
+    <Route path="/notebooks/:id" component={Notebook} />
   </div>
 );
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,20 @@ import 'typeface-metropolis';
 import './scss/custom.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Tranquill = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+ReactDOM.render(
+  <Tranquill />,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
