8a4a4d84692370021e1167acb119786f56c42349
Sruli <srulirapps@gmail.com>
2019-01-29 19:16:37 +0200

    Delete index.js

--- client/src/scenes/Notebook/index.js
+++
@@ -1,7 +0,0 @@
-import Component from './components';
-import { SCENE_PATH, SCENE_NAME } from './constants';
-import reducer from './reducer';
-import sagas from './sagas';
-
-export default Component;
-export { SCENE_PATH, SCENE_NAME, reducer, sagas };
