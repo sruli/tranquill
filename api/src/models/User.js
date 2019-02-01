fee150b0a71696ed5c74331e7224b27558427829
Sruli <srulirapps@gmail.com>
2019-02-01 15:03:28 +0200

    Trim email addresses before saving them

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -6,7 +6,7 @@ const { SALT_ROUNDS } = require('../../config/bcrypt');
 const emailRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, match: emailRegex, index: true },
+  email: { type: String, required: true, match: emailRegex, index: true, trim: true },
   password: { type: String, minlength: 8, maxlength: 72 },
   firstName: String,
 }, {
