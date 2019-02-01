5c1f711af74db835310f2afa72d1b94092d9c2cd
Sruli <srulirapps@gmail.com>
2019-02-01 15:57:32 +0200

    create user -> notebooks relationship

--- api/test/factories/notebookFactory.js
+++ api/test/factories/notebookFactory.js
@@ -1,8 +1,14 @@
 const { factory } = require('factory-girl');
+const userFactory = require('./userFactory');
 const Notebook = require('../../src/models/Notebook');
 
-factory.define('notebook', Notebook, {
-  name: 'notebook name',
+factory.define('notebook', Notebook, async () => {
+  const user = await userFactory.create('user');
+
+  return {
+    user,
+    name: 'Notebook',
+  };
 });
 
 module.exports = factory;
