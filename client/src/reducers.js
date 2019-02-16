241469bd87541ec3aa73d18edea72e2ca310c464
Sruli <srulirapps@gmail.com>
2019-02-16 18:35:15 +0200

    Remove config service

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -1,11 +1,9 @@
 import { SCENE_NAME as notebook, reducer as notebookReducer } from './scenes/notebook';
 import { SCENE_NAME as signIn, reducer as signInReducer } from './scenes/sign-in';
-import { SERVICE_NAME as config, reducer as configReducer } from './services/config';
 
 const reducers = {
   [notebook]: notebookReducer,
   [signIn]: signInReducer,
-  [config]: configReducer,
 };
 
 export default reducers;
--- client/src/services/config/index.js
+++
@@ -1,4 +0,0 @@
-import { SERVICE_NAME } from './constants';
-import reducer from './reducer';
-
-export { SERVICE_NAME, reducer };
