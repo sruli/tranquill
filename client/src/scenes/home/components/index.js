cf8a38bfd2bf0108a8b806fe6ab52552e52d23ae
Sruli <srulirapps@gmail.com>
2019-02-26 13:02:07 +0700

    Hide signup on home page and add basic responsive adjustments

--- client/src/scenes/home/components/index.js
+++ client/src/scenes/home/components/index.js
@@ -8,22 +8,29 @@ const Home = () => (
   <React.Fragment>
     <Navbar />
 
-    <div className="container-fluid min-vh-100 pt-md-5">
+    <div className="container-fluid min-vh-100 pt-5">
       <div className="row">
-        <div className="col-5 offset-1 pt-md-5">
+        <div className="col-md-5 offset-md-1 pt-5">
           <div className="row mb-md-5">
             <div className="col">
-              <h1 className={`display-1 ls-md-5 ${styles.display}`}>It&#39;s just a notebook—</h1>
+              <h1 className={`display-1 ls-5 ${styles.display}`}>It&#39;s just a notebook—</h1>
             </div>
           </div>
           <div className="row mb-md-5">
+            <div className="col">
+              <h3 className="font-weight-normal">
+                Coming soon…
+              </h3>
+            </div>
+          </div>
+          <div className="row mb-md-5 d-none">
             <div className="col">
               <h3 className="font-weight-normal">
                 Thoughts. Ideas. Lyrics. Poetry. Whatever you want. Everything is encrypted so what you write is yours alone.
               </h3>
             </div>
           </div>
-          <div className="row mb-md-5">
+          <div className="row mb-md-5 d-none">
             <div className="col">
               <form>
                 <div className="form-row align-items-end">
@@ -37,12 +44,12 @@ const Home = () => (
               </form>
             </div>
           </div>
-          <div className="row mb-md-4">
+          <div className="row mb-md-4 d-none">
             <div className="col">
               <h5 className="font-weight-normal">Can&#39;t hold that thought? Scroll down and get started.</h5>
             </div>
           </div>
-          <div className="row">
+          <div className="row d-none">
             <div className="col d-flex justify-content-center">
               <ArrowDownCircle />
             </div>
