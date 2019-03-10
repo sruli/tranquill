20b4d03138961bd1b0b6f5896c24fece273ed3a7
Sruli <srulirapps@gmail.com>
2019-03-10 07:52:50 +0200

    Remove mailchimp api key logging

--- api/lib/mailchimp.js
+++ api/lib/mailchimp.js
@@ -2,7 +2,6 @@ const fetch = require('node-fetch');
 
 const baseURL = 'https://us20.api.mailchimp.com/3.0';
 
-console.log('MAILCHIMP_API_KEY:', process.env.MAILCHIMP_API_KEY);
 const subscribe = async function subscribe({ email }) {
   const response = await fetch(`${baseURL}/lists/6006b2d7ac/members`, {
     method: 'POST',
